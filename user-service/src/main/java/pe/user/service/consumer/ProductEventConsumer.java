6484df7ca80164595933b1f9d3e7305958036800
Carlos Fausto Huaman Rengifo <carlos.huaman@grupomok.com>
2026-04-17 17:55:01 -0500

    cambios

--- user-service/src/main/java/pe/user/service/consumer/ProductEventConsumer.java
+++ user-service/src/main/java/pe/user/service/consumer/ProductEventConsumer.java
@@ -3,21 +3,17 @@ package pe.user.service.consumer;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
+import pe.user.service.config.RabbitMQConfig;
 import pe.user.service.event.ProductCreatedEvent;
 
 @Component
 public class ProductEventConsumer {
 
-    @RabbitListener(queues = "product.created.queue")
-    public void consume(ProductCreatedEvent event) {
+    @RabbitListener(queues = RabbitMQConfig.QUEUE)
+    public void handleProductCreated(ProductCreatedEvent event) {
 
-        System.out.println("📥 USER-SERVICE RECIBIÓ EVENTO:");
+        System.out.println("📥 EVENTO RECIBIDO:");
         System.out.println("Producto: " + event.getName());
-        System.out.println("UserId: " + event.getUserId());
-
-        // Aquí luego puedes hacer lógica real:
-        // - validar usuario
-        // - guardar historial
-        // - notificaciones
+        System.out.println("Usuario: " + event.getUserId());
     }
 }
